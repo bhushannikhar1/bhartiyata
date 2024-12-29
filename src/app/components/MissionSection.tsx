82635afcebeddea1ae0cdf68e0df8d91ea90a2da
Bhushan Nikhar <bhushannikhar@gmail.com>
2024-12-29 17:52:51 +0530

    fix: add name

--- src/app/components/MissionSection.tsx
+++ src/app/components/MissionSection.tsx
@@ -15,6 +15,7 @@ const MissionSection: React.FC = () => {
           heading: "Our Mission",
           subheading: "Bhartiyata - Indianization of learning",
           missionStatement: `At Bhartiyata, my mission is to empower and uplift the Indian community by providing accessible, high-quality skill development content in native languages. I recognize the challenges of learning with a language barrier and aim to bridge that gap by offering resources in languages that resonate with our people. My goal is to equip individuals with in-demand, productive skills that not only enhance their personal growth but also increase their earning potential. This initiative is my way of giving back to the nation, fostering self-reliance, and contributing to India's prosperity by unlocking the potential within every individual.`,
+          myname: "Bhushan Nikhar",
         },
       },
       2: {
@@ -23,6 +24,7 @@ const MissionSection: React.FC = () => {
           heading: "हमारा मिशन",
           subheading: "भारतीयता - अध्ययन का भारतीयकरण",
           missionStatement: `भारतीयता में, मेरा मिशन भारतीय समुदाय को सशक्त और प्रेरित करना है, जिससे हम अपनी मातृभाषाओं में उच्च गुणवत्ता वाले कौशल विकास सामग्री प्रदान कर सकें। मुझे यह अहसास है कि भाषा की बाधाओं के कारण सीखने में कठिनाई होती है, और मैं इसे इस उद्देश्य से दूर करना चाहता हूँ कि हम अपने लोगों के लिए ऐसी सामग्री प्रदान करें जो उनकी भाषा में हो। मेरा उद्देश्य व्यक्तियों को उच्च मांग वाले, उत्पादक कौशल प्रदान करना है, जो न केवल उनके व्यक्तिगत विकास को बढ़ाएं, बल्कि उनकी आय की संभावना भी बढ़ाएं। यह पहल मेरी तरफ से देश को कुछ वापस देने का एक तरीका है, आत्मनिर्भरता को बढ़ावा देने का, और भारत की समृद्धि के लिए हर व्यक्ति की संभावनाओं को उजागर करने का।`,
+          myname: "भूषण निखार",
         },
       },
       3: {
@@ -31,6 +33,7 @@ const MissionSection: React.FC = () => {
           heading: "आमचे मिशन",
           subheading: "भारतीयता - शिक्षणाचा भारतीयकरण",
           missionStatement: `भारतीयता मध्ये, माझं ध्येय भारतीय समुदायाला सशक्त आणि प्रगतीशील बनविणे आहे, ज्यामुळे स्थानिक भाषांमध्ये उच्च दर्जाचे कौशल्य विकास सामग्री प्रदान केली जाऊ शकते. मला माहितीये की भाषा अडथळ्यांमुळे शिकण्यामध्ये अडचणी येतात, आणि मी त्या अडचणींना दूर करण्यासाठी आसं कंटेंट तयार करण्याचा प्रयत्न करीत आहे जे आपल्या लोकांच्या भाषेत आहे. माझा उद्देश लोकांना मागणी असलेली आणि उत्पादक कौशल्ये शिकवण्याचा आहे, जे त्यांचा वैयक्तिक विकास वाढवेल आणि त्यांची कमाई क्षमता वाढवेल. हा माझा देशाची सेवा करण्याचा प्रयत्न आहे, आत्मनिर्भरतेला चालना देण्याचा, आणि भारताच्या समृद्धीसाठी प्रत्येक व्यक्तीच्या शक्यतांना खुलं करण्याचा आहे.`,
+          myname: "भूषण निखार",
         },
       },
     };
@@ -66,6 +69,7 @@ const MissionSection: React.FC = () => {
               height={120} // Height of the image
               className="mx-auto" // Removed margin-bottom from here
             />
+            <h3 className="text-xl text-gray-600 mt-2">{text.myname}</h3>
           </div>
         </div>
 
